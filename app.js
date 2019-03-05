486eadc43807b0ccd54ca01ed130e5c8f2f0f157
Roberto Sanchez <rsanchez305@gmail.com>
2019-03-04 20:26:08 -0500

    Configured to deploy application

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const session = require('express-session');
 const passportSetup = require('./config/passport/passport-setup');
 
 mongoose
-  .connect('mongodb://localhost/habit-tracker', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
